6c23b6f27ce2497ad4e3f6c9d9e78634081b6a76
Raymund Panganiban Etom <etom.raymund.panganiban@gmail.com>
2025-07-15 04:29:59 -0700

    First php file

---
+++ INcrementdecrement.php
@@ -0,0 +1,28 @@
+<?php
+
+// Increment, Decrement Operators
+
+
+// $counter = 0;
+$counter = 10;
+
+// $counter = $counter + 1;
+// $counter++;
+// $counter--;
+// $counter+=2;
+$counter-=3;
+
+
+echo "{$counter}<br>";
+
+
+// Operator procedure
+
+$total = null;
+
+
+
+$total = 1 + 2 - 3 * 4 / 5 ** 6;
+
+echo "\$ $total";
+?>
