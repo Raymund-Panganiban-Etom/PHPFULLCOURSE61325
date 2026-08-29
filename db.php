09478cbfdae046c8e70a75cdf6b7653063e8a892
Raymund <211331543+Raymund-Panganiban-Etom@users.noreply.github.com>
2026-08-29 03:32:08 -0700

    Refactor database connection code in db.php
    Updated connection messages and removed commented-out code.

--- db.php
+++ db.php
@@ -2,17 +2,30 @@
 
 
 
+// $db_server = "localhost";
+// $db_user = "root";
+// $db_pass = "";
+// $db_name = "test";
+// $connect = "";
+
+// try{
+//     $connect = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+//     echo "Connected<br>";
+// }catch(mysqli_sql_exception){
+//     echo "Not Connected<br>";
+// }
+
 $db_server = "localhost";
 $db_user = "root";
 $db_pass = "";
 $db_name = "test";
-$connect = "";
 
 try{
     $connect = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
-    echo "Connected<br>";
+    echo "Connected";
 }catch(mysqli_sql_exception){
-    echo "Not Connected<br>";
+    echo " Not Connected";
 }
+
 
 ?>
