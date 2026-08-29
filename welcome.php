629d1e8116b0116828909f9aef2f32c4ad6baaae
Raymund <211331543+Raymund-Panganiban-Etom@users.noreply.github.com>
2026-08-29 03:29:35 -0700

    Refactor welcome.php for session handling and layout

--- welcome.php
+++ welcome.php
@@ -1,23 +1,42 @@
 <?php
 
+// session_start();
+// echo "WELCOME " . $_SESSION['UserFSDFA'] . "<br>";
+
 session_start();
-echo " WELCOME " . $_SESSION['username'] . " <br>";
+echo "WELCOME " . $_SESSION['USER'] . "<br>";
 
+ ?>
 
-?>
+ <!-- <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+</head>
+ <body>
+     <form action="welcome.php" method="post">
+         <input type="submit" value="Logout" name="Logout">
+     </form>
+    
+ </body>
+</html> -->
+
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dashboard</title>
 </head>
 <body>
-    <form action="welcome.php" method="post">
-        <input type="submit" value="Logout" name="Logout">
-    </form>
     
+<form action="welcome.php" method="post">
+    <input type="submit" value="Logout" name="Logout">
+</form>
+
 </body>
 </html>
 
@@ -25,18 +44,24 @@ echo " WELCOME " . $_SESSION['username'] . " <br>";
 <?php
 
 
-include("db.php");
+// include("db.php");
 
-$getdata = "SELECT * FROM practice ";
-$result = mysqli_query($connect, $getdata);
-if(mysqli_num_rows($result) > 0){
-    $row = mysqli_fetch_assoc($result);
-    echo "User " . $row['User'] . "<br>";
- }
+// $getdata = "SELECT * FROM practice ";
+// $result = mysqli_query($connect, $getdata);
+// if(mysqli_num_rows($result) > 0){
+//     $row = mysqli_fetch_assoc($result);
+//     echo "User " . $row['User'] . "<br>";
+    
+//  }
+
+// if(isset($_POST['Logout'])){
+//     session_destroy();
+//     Header("Location: login.php");
+// }
 
 if(isset($_POST['Logout'])){
     session_destroy();
-    Header("Location: login.php");
+    header("Location: login.php");
 }
 
 ?>
